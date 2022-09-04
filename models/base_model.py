6dffbe2aee45c1162d8b35b3aa2b8577217a0390
Mambo Innocent <saneugene847@gmail.com>
2022-09-04 04:17:16 -0700

    update models

--- models/base_model.py
+++ models/base_model.py
@@ -1,66 +1,58 @@
-#!/usr/bin/python3
+#!/usr/bin/pyhon3
 """
-base class file
+Parent class that will inherit
 """
-
 import uuid
 from datetime import datetime
-import models
-from json import JSONEncoder
+from models import storage
 
 
 class BaseModel:
+    """Defines all common attributes/methods
     """
-    =========
-    BaseModel
-    =========
-    """
-
     def __init__(self, *args, **kwargs):
-        """initialize the instance of the class"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(
-                            value,
-                            '%Y-%m-%dT%H:%M:%S.%f')
-                elif key == "__class__":
-                    continue
-
-                setattr(self, key, value)
-        else:
+        """initializes all attributes
+        """
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-
-    def save(self):
-        """save new informations to the class object"""
-        self.updated_at = datetime.now()
-        models.storage.save()
-
-    def to_dict(self):
-        """return dictionary representaton of the instance"""
-        dict_repr = {}
-        for key, value in self.__dict__.items():
-            dict_repr[key] = value
-            if isinstance(value, datetime):
-                dict_repr[key] = value.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        dict_repr["__class__"] = type(self).__name__
-        return dict_repr
+            self.updated_at = self.created_at
+            storage.new(self)
+        else:
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
-        """return the string formated message when instance is called"""
-        clName = self.__class__.__name__
-        return "[{}] ({}) {}".format(clName, self.id, self.__dict__)
+        """returns class name, id and attribute dictionary
+        """
+        class_name = "[" + self.__class__.__name__ + "]"
+        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+        return class_name + " (" + self.id + ") " + str(dct)
 
+    def save(self):
+        """updates last update time
+        """
+        self.updated_at = datetime.now()
+        storage.save()
 
-class BaseModelEncoder(JSONEncoder):
-    """JSON Encoder for BaseModel
-    """
+    def to_dict(self):
+        """creates a new dictionary, adding a key and returning
+        datemtimes converted to strings
+        """
+        new_dict = {}
 
-    def default(self, o):
-        """ default"""
-        if isinstance(o, BaseModel):
-            return o.to_dict()
-        return super().default(o)
+        for key, values in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                if not values:
+                    pass
+                else:
+                    new_dict[key] = values
+        new_dict['__class__'] = self.__class__.__name__
+
+        return new_dict
